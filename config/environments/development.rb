43ad980734f11e43d6b0962c01ffc08a29b79def
olkm-ren-sasaki <sasaki.ren.infratop@gmail.com>
2023-12-25 15:53:55 +0900

    [add]devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  
+  # パスワード再設定用の設定
+  host = 'localhost:2011'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.raise_delivery_errors = true # メール送信時にエラーが発生した場合、エラーを表示
+  config.action_mailer.delivery_method = :smtp # メール送信の方法をSMTP経由に設定
+  config.action_mailer.smtp_settings = { # SMTPの設定
+    :port => 587,
+    :domain => 'gmail.com',
+    :address => "smtp.gmail.com",
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
+    :authentication => :plain, # 認証方式としてPLAINを使用
+    :enable_starttls_auto => true # STARTTLS（Transport Layer Security）を自動的に有効にする設定(セキュアな通信)
+  }
+
 
   config.action_mailer.perform_caching = false
 
